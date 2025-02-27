260ef204de2fa0e5510c81539285ed780e2f001b
Juyeong Seo <sjy3@wise-automotive.com>
2025-02-27 10:06:55 +0900

    backup

--- src/convert_pcd2bin.py
+++ src/convert_pcd2bin.py
@@ -30,4 +30,7 @@ def pcd_to_bin(pcd_file, bin_file):
 # 예시 사용법
 pcd_file = '/home/wise/workspace/ws_sensors/369-895235000.pcd'  
 bin_file = '/home/wise/Documents/sjy/250122_from_bag/output_images_and_pcds/4735.bin'  
+
+print(f'source: {pcd_file}')
+print(f'target: {bin_file}')
 pcd_to_bin(pcd_file, bin_file)
