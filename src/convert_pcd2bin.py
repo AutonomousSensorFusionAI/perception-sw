a90842c1b8700775990ebf1f406a8fcc37bde229
Juyeong Seo <sjy3@wise-automotive.com>
2025-04-25 08:49:45 +0900

    recycle bin

--- src/convert_pcd2bin.py
+++ src/convert_pcd2bin.py
@@ -29,6 +29,9 @@ def pcd_to_bin(pcd_file, bin_file):
     # bin 파일로 변환하여 저장 (float32 타입으로 저장)
     data.astype(np.float32).tofile(bin_file)
     print(f"Saved binary file: {bin_file}")
+
+
+
 # 예시 사용법
 pcd_file = '/home/wise/workspace/ws_sensors/369-895235000.pcd'  
 bin_file = '/home/wise/Documents/sjy/250122_from_bag/output_images_and_pcds/4735.bin'  
