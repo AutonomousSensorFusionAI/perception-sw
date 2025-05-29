e5a7989c1ef6a8dd755da4e97a65c07b0f180b3b
msc <msc@wise-automotive.com>
2025-05-29 10:46:24 +0900

    lidar camera 알고리즘 변경

--- src/lidar_camera.cpp
+++ src/lidar_camera.cpp
@@ -72,6 +72,7 @@ int main(int argc, char *argv[]) {
             calib_file = data_folder + '/' + ptr->d_name;
         ptr++;
     }
+    
 
     auto time_begin = std::chrono::steady_clock::now();
     Calibrator calibrator(mask_dir, lidar_file, calib_file, img_file, error_file);
