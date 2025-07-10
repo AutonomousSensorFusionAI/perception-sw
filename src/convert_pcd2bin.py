e3db5a5a7a8ec16f888c16955e1211e84d48bae6
msc <msc@wise-automotive.com>
2025-07-10 10:02:50 +0900

    데이터 로깅 시스템 점검

--- src/convert_pcd2bin.py
+++ src/convert_pcd2bin.py
@@ -23,6 +23,7 @@ def pcd_to_bin(pcd_file, bin_file):
     print(f"Saved binary file: {bin_file}")
 
 
+
 # 예시
 pcd_file = '/home/wise/workspace/ws_sensors/369-895235000.pcd'  
 bin_file = '/home/wise/Documents/sjy/250122_from_bag/output_images_and_pcds/4735.bin'  
